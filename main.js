055dee5e1403f0ff613c4b3c708e423c37a2ec4e
Morgan Shepard <morgan@Morgans-MBP.lan>
2018-03-13 14:43:42 -0400

    Yarp

---
+++ main.js
@@ -0,0 +1,41 @@
+const main = document.getElementById("main");
+
+var coin = {
+    state: 0,
+    flip: function () {
+        this.state = Math.round(Math.random());
+        console.log(this.state);
+        // randomly set this.state to be either 0 or 1 √√√
+    },
+    toString: function () {
+        var message = document.createElement("div");
+        if (this.state == 0) {
+            return("H");
+        }
+        else if (this.state == 1) {
+            return("T");
+        }
+        // return “H” or “T” depending on whether this.state is 0 or 1 √√√
+    },
+    toHTML: function () {
+        var img = document.createElement("img")
+        if (this.toString() == "H") {
+            img.classList = "heads";
+        }
+
+        if (this.toString() == "T") {
+            img.classList = "tails";            
+        }
+        main.appendChild(img);
+        // set the properties of the image element to show either heads or tails;
+}
+};
+
+for (i = 0; i < 20; i++) {
+    coin.flip();
+    coin.toString();
+    coin.toHTML();
+}
+// for (j = 0; j < 20; i++) {
+//     coin.toHTML();
+// }
